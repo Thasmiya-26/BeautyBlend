eeef9f36e75c629a4d606ec0a95421a776a2fad2
Thasmiya <jazminejazmine173@gmail.com>
2026-09-05 13:51:39 +0530

    Fix Render static files and settings

--- BeautyBlend/settings.py
+++ BeautyBlend/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "django-insecure-((bt#8q6ya_8wq7g(3sd&@a8-@@y@kzg4(q-k&bp+aqsevr5p3"
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # =========================================================
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     "django.middleware.security.SecurityMiddleware",
+
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
     "django.contrib.sessions.middleware.SessionMiddleware",
 
@@ -178,13 +180,32 @@ USE_TZ = True
 # STATIC FILES
 # =========================================================
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
 
     BASE_DIR / "static",
 
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# =========================================================
+# WHITE NOISE
+# =========================================================
+
+STORAGES = {
+
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+
+}
 
 
 # =========================================================
@@ -201,3 +222,10 @@ MAILERS = {
     },
 
 }
+
+
+# =========================================================
+# DEFAULT PRIMARY KEY FIELD TYPE
+# =========================================================
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
